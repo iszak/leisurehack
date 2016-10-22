f29e93570c6d27454e47fc8c801573c4b23def93
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2016-10-22 17:58:53 +0100

    Created frontend player sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import PlayerSignUp from './views/player-sign-up';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+        <MuiThemeProvider>
+            <PlayerSignUp />
+        </MuiThemeProvider>
     );
   }
 }
