7deaca973b59e1249ab521c4518bf85817921183
Iszak Bryan <iszak.bryan@gmail.com>
2016-10-22 18:01:33 +0100

    Added create team route

--- api/routes/teams.js
+++ api/routes/teams.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const Team = require('../models/index').team
 
 router.post('/', (req, res, next) => {
-
+  Team.create(req.body).then(team => {
+    res.status(201);
+    res.send(team);
+  }, error => {
+    res.status(400);
+    res.send(error);
+  })
 });
 
 module.exports = router;
