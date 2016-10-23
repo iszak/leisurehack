862c929bb757102c6d1747956bbbdde7dc333218
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2016-10-23 06:27:34 +0100

    Added tab logic to find player view

--- client/src/views/dashboard-views/look-for-player.js
+++ client/src/views/dashboard-views/look-for-player.js
@@ -4,6 +4,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
 import TimePicker from 'material-ui/TimePicker';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import AutoComplete from 'material-ui/AutoComplete';
 import curry from 'lodash/fp/curry';
 
 
@@ -11,13 +13,21 @@ const paperStyle = {
     padding: 20
 };
 
+const players = [
+    'John Bishop',
+    'Ben Robinson',
+    'Charles Smith', 
+    'Gordon Lovesgood'
+];
+
 export default class LookForPlayer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             players: 0,
             date: '',
-            time: ''
+            time: '',
+            tab: 'a'
         };
     }
 
@@ -45,34 +55,63 @@ export default class LookForPlayer extends Component {
         });
     });
 
+    tabChange = (value) => {
+        this.setState(previousState => {
+            previousState.tab = value;
+            return previousState
+        });
+    };
+
     render() {
         return (
             <Paper style={paperStyle} zDepth={1}>
                 <h1>Look for player</h1>
-                <p>Fill in the details below and click the link to log into Facebook and post a player request for an upcoming game</p>
-                <br />
-                <p>How many player are you looking for?</p>
-                <TextField
-                    hintText='2'
-                    floatingLabelText='Number of players'
-                    floatingLabelFixed={true}
-                    type='number'
-                    onChange={this.textChange('players')}
-                />
-                <p>What date is the game on?</p>
-                <DatePicker
-                    hintText="Date"
-                    floatingLabelText='Date'
-                    onChange={this.dateChange('date')}
-                />
-                <p>What time is the game?</p>
-                <TimePicker
-                    hintText='Time'
-                    floatingLabelText='Time'
-                    onChange={this.dateChange('time')} 
-                />
-                <br/>
-                <RaisedButton label="Post player request" secondary={true} onClick={this.myFacebookLogin} />
+                <Tabs
+                    value={this.state.tab}
+                    onChange={this.tabChange}
+                >
+                    <Tab label="Search for player" value="a">
+                        <div>
+                            <p>Use the search below to find existing players to play for your team.</p>
+                            <br />
+                            <AutoComplete
+                                floatingLabelText="Player name"
+                                filter={AutoComplete.fuzzyFilter}
+                                dataSource={players}
+                                maxSearchResults={5}
+                                onNewRequest={this.onSearch}
+                            />
+                        </div>
+                    </Tab>
+                    <Tab label="Post for player" value="b">
+                        <div>
+                            <p>Fill in the details below and click the link to log into Facebook and post a player request for an upcoming game</p>
+                            <br />
+                            <p>How many player are you looking for?</p>
+                            <TextField
+                                hintText='2'
+                                floatingLabelText='Number of players'
+                                floatingLabelFixed={true}
+                                type='number'
+                                onChange={this.textChange('players')}
+                            />
+                            <p>What date is the game on?</p>
+                            <DatePicker
+                                hintText="Date"
+                                floatingLabelText='Date'
+                                onChange={this.dateChange('date')}
+                            />
+                            <p>What time is the game?</p>
+                            <TimePicker
+                                hintText='Time'
+                                floatingLabelText='Time'
+                                onChange={this.dateChange('time')} 
+                            />
+                            <br/>
+                            <RaisedButton label="Post player request" secondary={true} onClick={this.myFacebookLogin} />
+                        </div>
+                    </Tab>
+                </Tabs>
             </Paper>
         );
     }
